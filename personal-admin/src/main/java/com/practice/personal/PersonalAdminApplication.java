325d238ea42816e31852b4dd81878dddebe5c7f6
cailin <741978260@qq.com>
2018-12-28 00:17:32 +0800

    新增

---
+++ personal-admin/src/main/java/com/practice/personal/PersonalAdminApplication.java
@@ -0,0 +1,14 @@
+package com.practice.personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalAdminApplication.class, args);
+    }
+
+}
+
