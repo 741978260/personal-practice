c4b644c12a8b0c69c82586fb25439f534d31029d
cailin <741978260@qq.com>
2019-02-18 11:13:16 +0800

    解决：在fenzhi1添加etest模块，进行不同分支测试

---
+++ personal-etest/src/main/java/com/practice/personal/PersonalEtestApplication.java
@@ -0,0 +1,13 @@
+package com.practice.personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalEtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalEtestApplication.class, args);
+    }
+
+}
